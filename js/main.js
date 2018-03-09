5fba18bf86d9db55293b372292e5cc25e5bd8e43
Matt Silveira <Matts_Home@Matthews-MacBook-Air.local>
2018-03-09 14:41:51 -0700

    added CB's and legend

--- js/main.js
+++ js/main.js
@@ -1,21 +1,96 @@
 // #########################################################
 // main js file for the Hudson River web mapping application
-require([ "esri/map", "esri/dijit/Search", "dojo/domReady!"
-  ], function (Map, Search) {
-  	// esri map
+// ESRI api functions ///////////////////////////////////////////////////////////////////////////////////////////////////
+// esri api calls
+var app = {}; // main app object
+app.visibleLayers = [0,2,3,4]
+require(["esri/map", "esri/layers/ArcGISDynamicMapServiceLayer", "esri/tasks/query", "esri/tasks/QueryTask", "esri/symbols/TextSymbol",
+    "esri/symbols/Font", "esri/Color", "esri/geometry/Extent", "esri/layers/FeatureLayer", "esri/symbols/SimpleFillSymbol", "esri/symbols/SimpleLineSymbol",
+        "esri/renderers/SimpleRenderer", "esri/graphic", "esri/dijit/Search", "dojo/domReady!","esri/dijit/Legend"], 
+function(Map, ArcGISDynamicMapServiceLayer, Query, QueryTask, TextSymbol, Font, Color, Extent, FeatureLayer, SimpleFillSymbol, SimpleLineSymbol,
+        SimpleRenderer, Graphic, Search, Legend) {
+  	// esri map//////////////////////////////////////////////////////////////////////////////////////////////////////
      var map = new Map("map", {
         basemap: "topo",  //For full list of pre-defined basemaps, navigate to http://arcg.is/1JVo6Wd
         center: [-73.9, 42.05], // lon, lat
         zoom: 8,
         sliderPosition: "top-right"
      });
+
      // code for ESRI search
      var search = new Search({
         map: map
      }, "search");
      search.startup();
+     map.on("load", function() {
+        console.log('inside load')
+        map.enableScrollWheel()
+     })
+     // add dynamic layer to map
+     var url = "http://nyspatial.tnc.org:6080/arcgis/rest/services/HudsonRiverWebServices/HudsonRiverMapService_v03082018/MapServer";
+     // Add dynamic map service
+    var dynamicLayer = new ArcGISDynamicMapServiceLayer(url, {opacity:0.7});
+    
+    // on dynamic layer load ////////////////////////////////////////////////////////////////////////////////////////////////
+    dynamicLayer.on("load", function () {
+        console.log('loaded');
+        dynamicLayer.setVisibleLayers(app.visibleLayers);
+        map.addLayer(dynamicLayer);
+        console.log('added layer')
+
+    });
+
+    // //add the legend ////////////////////////////////////////////////////////////////////////////////////////
+    var legendLayers = [{ layer: dynamicLayer }]
+    map.on('layers-add-result', function () {
+        console.log('inside')
+      var legend = new Legend({
+        map: map,
+        // layerInfos: legendLayers
+      }, "legendDiv");
+      legend.startup();
+    });
+    
 
 }); // end of main require function
+
+
+
+
+
+
+
+
+
+
+
+
+
+// when document is ready //////////////////////////////////////////////////////////////////////////////////////////////////
+$( document ).ready(function() {
+    // on doc click
+    $(document).on('click', function (e) {
+    // Do whatever you want; the event that'd fire if the "special" element has been clicked on has been cancelled.
+        var c = $(e.target).attr('class')
+        if(c){
+             var i = c.indexOf('hamburgerClick')
+        }
+        if(i > -1){
+            $('.dropdownMenu').slideDown();
+        }else{
+            $('.dropdownMenu').slideUp();
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
 
 
 
