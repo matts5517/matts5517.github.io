5feee3c920eeb378ec0d7a252c65e331b5c60138
Matt Silveira <Matts_Home@Matthews-MacBook-Air.local>
2018-03-12 14:55:41 -0600

    push from dev box

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 // ESRI api functions ///////////////////////////////////////////////////////////////////////////////////////////////////
 // esri api calls
 var app = {}; // main app object
-app.visibleLayers = [0,2,3,4]
+app.visibleLayers = [0,4]
 require(["esri/map", "esri/layers/ArcGISDynamicMapServiceLayer", "esri/tasks/query", "esri/tasks/QueryTask", "esri/symbols/TextSymbol",
     "esri/symbols/Font", "esri/Color", "esri/geometry/Extent", "esri/layers/FeatureLayer", "esri/symbols/SimpleFillSymbol", "esri/symbols/SimpleLineSymbol",
         "esri/renderers/SimpleRenderer", "esri/graphic", "esri/dijit/Search", "dojo/domReady!","esri/dijit/Legend"], 
@@ -27,63 +27,90 @@ function(Map, ArcGISDynamicMapServiceLayer, Query, QueryTask, TextSymbol, Font, 
         map.enableScrollWheel()
      })
      // add dynamic layer to map
+     // var url = "http://nyspatial.tnc.org:6080/arcgis/rest/services/HudsonRiverWebServices/HudsonRiverMapService_v03082018/MapServer";
+     // for github url
      var url = "https://nyspatial.tnc.org/arcgis/rest/services/HudsonRiverWebServices/HudsonRiverMapService_v03082018/MapServer";
+
      // Add dynamic map service
     var dynamicLayer = new ArcGISDynamicMapServiceLayer(url, {opacity:0.7});
     
     // on dynamic layer load ////////////////////////////////////////////////////////////////////////////////////////////////
     dynamicLayer.on("load", function () {
-        console.log('loaded');
         dynamicLayer.setVisibleLayers(app.visibleLayers);
-        map.addLayer(dynamicLayer);
-        console.log('added layer')
-
-    });
-
-    // //add the legend ////////////////////////////////////////////////////////////////////////////////////////
-    var legendLayers = [{ layer: dynamicLayer }]
-    map.on('layers-add-result', function () {
-        console.log('inside')
-      var legend = new Legend({
-        map: map,
-        // layerInfos: legendLayers
-      }, "legendDiv");
-      legend.startup();
+        map.addLayer(dynamicLayer); // add dynamic layer
     });
     
+    
+    
+
+
+
+
+    // //add the legend ////////////////////////////////////////////////////////////////////////////////////////
+    // var legendLayers = [{ layer: dynamicLayer }]
+    // map.on('layers-add-result', function () {
+    //     console.log('inside')
+    //   var legend = new Legend({
+    //     map: map,
+    //     // layerInfos: legendLayers
+    //   }, "legendDiv");
+    //   legend.startup();
+    // });
+    
+
+
+
+    // when document is ready //////////////////////////////////////////////////////////////////////////////////////////////////
+    // html code goes here
+    $( document ).ready(function() {
+        // on cb clicks add and remoce layers /////////////////
+        $('.cbWrapper input').click(function(c){
+            var layerId = parseInt(c.currentTarget.id.split('-')[1]);
+            if(c.currentTarget.checked){
+                app.visibleLayers.push(layerId)
+            }else{
+                var index = app.visibleLayers.indexOf(layerId)
+                if (index !== -1) app.visibleLayers.splice(index, 1);
+            }
+            dynamicLayer.setVisibleLayers(app.visibleLayers);
+        })
+        // header collapse functionality
+        $('.cbHeader').on('click', function(e){
+            // check to see the height of the next cbWrapper
+            // if its open close it, change text to show
+            if($(e.currentTarget).next().is(':visible')){
+                $(e.currentTarget).next().slideUp();
+                $(e.currentTarget).find('span').html('Show')
+            }else{ // else open it, chnage text to collapse
+                $(e.currentTarget).next().slideDown();
+                $(e.currentTarget).find('span').html('Collapse')
+            }
+        })
+         // header mouse over functionality
+        $('.cbHeader').on('mouseover', function(e){
+            // add blue font class to span
+            $(e.currentTarget).find('span').css('color', '#2F6384')
+        })
+        $('.cbHeader').on('mouseout', function(e){
+            $(e.currentTarget).find('span').css('color', '#f3f3f3')
+            // remove blue font class to span
+        })
+        // on doc click
+        $(document).on('click', function (e) {
+        // Do whatever you want; the event that'd fire if the "special" element has been clicked on has been cancelled.
+            var c = $(e.target).attr('class')
+            if(c){
+                 var i = c.indexOf('hamburgerClick')
+            }
+            if(i > -1){
+                $('.dropdownMenu').slideDown();
+            }else{
+                $('.dropdownMenu').slideUp();
+            }
+        });
+    });
 
 }); // end of main require function
-
-
-
-
-
-
-
-
-
-
-
-
-
-// when document is ready //////////////////////////////////////////////////////////////////////////////////////////////////
-$( document ).ready(function() {
-    // on doc click
-    $(document).on('click', function (e) {
-    // Do whatever you want; the event that'd fire if the "special" element has been clicked on has been cancelled.
-        var c = $(e.target).attr('class')
-        if(c){
-             var i = c.indexOf('hamburgerClick')
-        }
-        if(i > -1){
-            $('.dropdownMenu').slideDown();
-        }else{
-            $('.dropdownMenu').slideUp();
-        }
-    });
-});
-
-
 
 
 
